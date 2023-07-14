b2b1a41a3a92a568e9c8bc33b72c6d107b53201f
Vinitalhari <127136058+Talhari1@users.noreply.github.com>
2023-07-14 17:16:05 -0300

    Pergunta01.sql

--- parte1/exercicios/01.sql
+++ parte1/exercicios/01.sql
@@ -1,1 +1,14 @@
-select
+
+-- Quantos produtos temos da categoria esporte lazer?
+
+-- contagem
+select 
+        count(*) as qtd_linhas,
+        count(product_id) as qtd_ptodutos,
+        count(DISTINCT product_id) as qtd_produtos_distintos,
+        count(DISTINCT product_category_name) as qtd_categorias_distintos
+
+
+from tb_products 
+
+where product_category_name	= 'esporte_lazer'
